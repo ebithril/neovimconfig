fae306c62a2f58f6fc29844b0843c8623c0c78e0
Patrik Görling <p.gorling@gmail.com>
2023-12-16 19:22:28 +0100

    fixed some keymaps

--- lua/user/keymap.lua
+++ lua/user/keymap.lua
@@ -1,7 +1,12 @@
+local map = vim.keymap.set
 vim.g.mapleader = ','
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>o', builtin.find_files, {})
-vim.keymap.set('n', '<leader>s', builtin.lsp_workspace_symbols, {})
-vim.keymap.set('n', '<leader>f', builtin.lsp_document_symbols, {})
+map('n', '<leader>o', builtin.find_files, {})
+map('n', '<leader>s', builtin.lsp_workspace_symbols, {})
+map('n', '<leader>f', builtin.lsp_document_symbols, {})
 
+-- FloaTerm configuration
+map('n', "<leader>t", ":FloatermNew --name=myfloat --height=0.8 --width=0.7 --autoclose=2 fish <CR> ")
+map('n', "t", ":FloatermToggle myfloat<CR>")
+map('t', "<Esc>", "<C-\\><C-n>:q<CR>")
